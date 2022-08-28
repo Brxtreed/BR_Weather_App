52141e9045517243095cf5dd816f0b5df83c6176
Brxtreed <Brxtreed@live.com>
2022-08-28 14:24:43 -0500

    Ignored the creation of database schema

--- app/src/main/java/com/weather/br_weather/Util/CityDatabase.kt
+++ app/src/main/java/com/weather/br_weather/Util/CityDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.weather.br_weather.dao.CityDao
 import com.weather.br_weather.model.City
 
-@Database(entities = [City::class], version = 1)
+@Database(entities = [City::class], version = 1, exportSchema = false)
 abstract class CityDatabase: RoomDatabase() {
     abstract fun cityDao(): CityDao
 
